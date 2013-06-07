6ce35d8722b86a10ff87ece482adec53eb5f3f55
Basil Shubin <basil.shubin@gmail.com>
2013-06-07 17:38:31 +0700

    initial commit

---
+++ crumbs/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+__all__ = ['CrumbsMixin']
+
+
+class CrumbsMixin(object):
+
+    def get_crumbs(self, context):
+        raise NotImplementedError
+
+    def render_to_response(self, context, **response_kwargs):
+        if hasattr(self.request, 'breadcrumbs'):
+            self.request.breadcrumbs(self.get_crumbs(context))
+            context['show_crumbs'] = True
+        else:
+            context['show_crumbs'] = False
+        return super(CrumbsMixin, self).render_to_response(context, **response_kwargs)
