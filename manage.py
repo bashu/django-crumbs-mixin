569c9158b391271fd09ae51b1bbb197d67069cb1
Basil Shubin <basil.shubin@gmail.com>
2014-08-18 13:13:32 +0700

    fix test suite to work with django 1.6

--- manage.py
+++ manage.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-import os
+import os, sys
 
-from django.core.management import execute_manager
+from django.core.management import ManagementUtility
 
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
@@ -12,8 +12,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': ':memory:'
-        }
     }
+}
 
 MIDDLEWARE_CLASSES = [
     'breadcrumbs.middleware.BreadcrumbsMiddleware',
@@ -30,28 +30,25 @@ TEMPLATE_CONTEXT_PROCESSORS = (
 
 PROJECT_APPS = [
     'crumbs',
-    ]
+]
 
 INSTALLED_APPS = [
     'breadcrumbs',
 
     'django_jenkins',
-    'discover_runner',
-    ] + PROJECT_APPS
+] + PROJECT_APPS
 
 ROOT_URLCONF = 'test_urls'
 
 TEMPLATE_DIRS = [
     os.path.join(PROJECT_ROOT, 'test_templates'),
-    ]
-
-TEST_RUNNER = 'discover_runner.DiscoverRunner'
+]
 
 JENKINS_TASKS = (
     'django_jenkins.tasks.run_flake8',
     'django_jenkins.tasks.run_pylint',
     'django_jenkins.tasks.with_coverage',
-    )
+)
 
 COVERAGE_EXCLUDES_FOLDERS = ['crumbs/tests/*']
 PYLINT_RCFILE = os.path.join(PROJECT_ROOT, 'pylint.rc')
@@ -63,7 +60,6 @@ if __name__ == "__main__":
         MIDDLEWARE_CLASSES = MIDDLEWARE_CLASSES,
         INSTALLED_APPS = INSTALLED_APPS,
         PROJECT_APPS = PROJECT_APPS,
-        TEST_RUNNER = TEST_RUNNER,
         JENKINS_TASKS = JENKINS_TASKS,
         ROOT_URLCONF = ROOT_URLCONF,
         COVERAGE_EXCLUDES_FOLDERS = COVERAGE_EXCLUDES_FOLDERS,
@@ -72,4 +68,5 @@ if __name__ == "__main__":
         TEMPLATE_CONTEXT_PROCESSORS = TEMPLATE_CONTEXT_PROCESSORS,
         TEMPLATE_DEBUG = TEMPLATE_DEBUG,
         )
-    execute_manager(settings)
+    utility = ManagementUtility(sys.argv)
+    utility.execute()
